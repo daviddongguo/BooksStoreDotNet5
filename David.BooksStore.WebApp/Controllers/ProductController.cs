ff1a030361c7cabf0b9f6445eeabcfefd14b0c79
daviddongguo <david.dong.guo@gmail.com>
2020-12-19 08:07:01 -0500

    Add product  controller

---
+++ David.BooksStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using David.BooksStore.Domain.Abstract;
+using David.BooksStore.Domain.Entities;
+using David.BooksStore.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace David.BooksStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {      
+        private readonly IProductsRepository _rep;
+        private readonly int PAGE_SIZE = 5;
+
+        public ProductController(IProductsRepository rep)
+        {
+            _rep = rep;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List(string category, int page = 1)
+        {
+            ProductsListViewModel productsModel = new ProductsListViewModel
+            {
+                // Filter the products 
+                Products = _rep
+                        .Products
+                        .Where(p => category == null || p.Category == category)
+                        .OrderBy(p => p.ProductId)
+                        .Skip((page - 1) * PAGE_SIZE)
+                        .Take(PAGE_SIZE),
+
+                // 
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PAGE_SIZE,
+                    // Get the products remaind
+                    TotalItems = _rep
+                            .Products
+                            .Where(p => category == null || p.Category == category)
+                            .Count()
+                },
+
+                CurrentCategory = category
+            };
+
+            ViewBag.productsModel = productsModel;
+            ViewBag.ReturnUrl = HttpContext.Request.Path + HttpContext.Request.QueryString;
+
+            return View(ViewBag);
+        }
+
+
+        public FileContentResult GetImage(int productId)
+        {
+            Product prod = _rep
+                .Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (prod != null)
+            {
+                return File(prod.ImageData, prod.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
