7907bc86e7e379e4739c82d11873c318308991e6
daviddongguo <david.dong.guo@gmail.com>
2020-12-19 12:08:24 -0500

    Pass options to DbContext constructor

--- David.BooksStore.DebugConsole/Program.cs
+++ David.BooksStore.DebugConsole/Program.cs
@@ -1,6 +1,9 @@
 ﻿using David.BooksStore.Domain.Concrete;
 using David.BooksStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System;
+using System.Configuration;
 
 namespace David.BooksStore.DebugConsole
 {
@@ -12,38 +15,45 @@ namespace David.BooksStore.DebugConsole
 
             //var p = CreateProduct();
             //System.Console.WriteLine(p.ToString());
+            //InsertData();
+            PrintData(args[0]);
+            
 
             Console.ReadKey();
 
         }
 
-        // private static void PrintData()
-        // {
-        //     using var context = new EFDbContext();
-        //     var products = context.Products;
-        //     foreach (var p in products)
-        //     {
-        //         Console.WriteLine(p.ToString());
-        //     }
-
-        // }
-        // private static void InsertData()
-        // {
-        //     using var context = new EFDbContext();
-        //     // Creates the database if not exists
-        //     context.Database.EnsureCreated();
-
-        //     // Adds a product
-        //     var product = CreateProduct();
-        //     context.Products.Add(product);
-
-        //     // Saves changes
-        //     context.SaveChanges();
-        // }
-
-        private static Product CreateProduct()
+        private static void PrintData(string connectionString)
         {
-            int i = 2;
+            var options = new DbContextOptionsBuilder<EFDbContext>()
+                        .UseMySQL(connectionString)
+                        .Options;
+
+            using var context = new EFDbContext(options);
+            var products = context.Products;
+            foreach (var p in products)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+        }
+        //private static void InsertData()
+        //{
+        //    using var context = new EFDbContext();
+        //    // Creates the database if not exists
+        //    context.Database.EnsureCreated();
+
+        //    // Adds a product
+        //    for(int i = 3; i <= 51; i++)
+        //    {               
+        //        context.Products.Add(CreateProduct(i));
+        //    }
+        //    // Saves changes
+        //    context.SaveChanges();
+        //}
+
+        private static Product CreateProduct(int i)
+        {            
             return new Product
             {
                 Title = "Think in " + i,
