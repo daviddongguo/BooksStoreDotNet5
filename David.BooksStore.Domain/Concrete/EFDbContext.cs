6d2e45d17cdab20fe8bea77a6d278f620429f6c7
daviddongguo <david.dong.guo@gmail.com>
2020-12-18 20:54:41 -0500

    Upgrade EFDbContext constructor by using DbContextOptions

--- David.BooksStore.Domain/Concrete/EFDbContext.cs
+++ David.BooksStore.Domain/Concrete/EFDbContext.cs
@@ -10,10 +10,16 @@ namespace David.BooksStore.Domain.Concrete
 {
     public class EFDbContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      optionsBuilder.UseMySQL("server=bvakwjqxl1kivtlftlkl-mysql.services.clever-cloud.com;user id=uih9cosqpakteeln;pwd=cGaRGjCKajlUyz8fautM;persistsecurityinfo=True;database=bvakwjqxl1kivtlftlkl");
-    }
+        //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //  optionsBuilder.UseMySQL("server=bvakwjqxl1kivtlftlkl-mysql.services.clever-cloud.com;user id=uih9cosqpakteeln;pwd=cGaRGjCKajlUyz8fautM;persistsecurityinfo=True;database=bvakwjqxl1kivtlftlkl");
+        //}
+        public EFDbContext() : base()
+        {
+        }
+        public EFDbContext(DbContextOptions<EFDbContext> options) : base(options)
+        {
+        }
         // Declare a list
         public DbSet<Product> Products { get; set; }
 
