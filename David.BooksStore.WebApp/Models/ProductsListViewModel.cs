84a5eff6b9fe067052ce81457059d35fbeddb712
daviddongguo <david.dong.guo@gmail.com>
2020-12-19 09:13:00 -0500

    Add products list view model

---
+++ David.BooksStore.WebApp/Models/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using David.BooksStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace David.BooksStore.WebApp.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
